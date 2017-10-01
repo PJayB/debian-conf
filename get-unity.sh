eef11a01a6541aa53674a082a0d251f008e8321c
Pete Lewis <mygit@pjblewis.com>
2017-09-30 19:47:01 -0700

    Update get-unity to new Unity download manager

--- get-unity.sh
+++ get-unity.sh
@@ -16,12 +16,12 @@ fi
 
 if [ "$1" == "" ]; then
 	echo "Please get latest URL of public_download.html from the Forums!"
-	echo "https://forum.unity3d.com/threads/unity-on-linux-release-notes-and-known-issues.350256/"
+	echo "https://forum.unity3d.com/threads/unity-on-linux-release-notes-and-known-issues.350256/page-2"
 	exit 1
 fi
 
 UNITYURL=$1
-INSTALLER=$(curl $UNITYURL | grep -i -o -E "(http[^'>]*\.deb)")
+INSTALLER=$(curl $UNITYURL | grep -i -o -E "(http[^\"'>]*UnitySetup[^\"'>]*)")
 if [ $? -ne 0 ]; then
 	echo "Failed to acquire download URL from $UNITYURL"
 	exit 1
@@ -33,17 +33,18 @@ fi
 
 INSTALLER_FILE=$(echo ${INSTALLER} | grep -i -o -E "[^/]+$")
 
+echo Installing dependencies...
+sudo apt-get install gtk2 libsoup libarchive
+
 echo Installing from $INSTALLER_FILE...
 
 if [ ! -f $INSTALLER_FILE ]; then
 	wget $INSTALLER
 fi
 
-sudo dpkg -i $INSTALLER_FILE
+#sudo dpkg -i $INSTALLER_FILE
+chmod +x $INSTALLER_FILE
+echo y | sudo ./$INSTALLER_FILE --unattended --install-location=/opt
 if [ $? -eq 0 ]; then
-	sudo apt-get --fix-broken install
 	rm $INSTALLER_FILE
 fi
-
-
-
