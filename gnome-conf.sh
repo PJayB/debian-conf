ee8f2777e53400d32f1e2e52a3d867c9b4d65026
Pete Lewis <petele@unity3d.com>
2017-11-02 18:08:51 -0700

    More gnome tools

--- gnome-conf.sh
+++ gnome-conf.sh
@@ -1,3 +1,3 @@
-sudo apt install gdm3 gnome-panel gnome-control-center gnome-tweak-tool
+sudo apt install -y gdm3 gnome-panel gnome-control-center gnome-tweak-tool gparted baobab gnome-system-log gnome-screenshot
 sudo dpkg-reconfigure gdm3
 
