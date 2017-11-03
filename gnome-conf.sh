0799a514c96e6d90efd8d0477f4baea85bbb90d8
Pete Lewis <petele@unity3d.com>
2017-11-03 15:02:36 -0700

    gnome-shell-extensions

--- gnome-conf.sh
+++ gnome-conf.sh
@@ -1,3 +1,3 @@
-sudo apt install -y gdm3 gnome-panel gnome-control-center gnome-tweak-tool gparted baobab gnome-system-log gnome-screenshot
+sudo apt install -y gdm3 gnome-panel gnome-control-center gnome-tweak-tool gparted baobab gnome-system-log gnome-screenshot gnome-shell-extensions
 sudo dpkg-reconfigure gdm3
 
