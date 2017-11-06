ced7aafee7fcf49505af249288ad7502d10c56f2
Pete Lewis <petele@unity3d.com>
2017-11-06 14:38:21 -0800

    Additional pkgs

--- gnome-conf.sh
+++ gnome-conf.sh
@@ -1,3 +1,3 @@
-sudo apt install -y gdm3 gnome-panel gnome-control-center gnome-tweak-tool gparted baobab gnome-system-log gnome-screenshot gnome-shell-extensions gnome-icon-theme-symbolic
+sudo apt install -y gdm3 gnome-panel gnome-control-center gnome-tweak-tool gparted baobab gnome-system-log gnome-screenshot gnome-shell-extensions gnome-icon-theme-symbolic gnome-disk-utility gnome-logs gnome-software gnome-shell-extension-multi-monitors gnome-shell-extension-show-ip
 sudo dpkg-reconfigure gdm3
 
