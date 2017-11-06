1c4b3eb149f81e7a7f0cc9dd8c739d296114d807
Pete Lewis <petele@unity3d.com>
2017-11-06 14:28:06 -0800

    Icons for gnome

--- gnome-conf.sh
+++ gnome-conf.sh
@@ -1,3 +1,3 @@
-sudo apt install -y gdm3 gnome-panel gnome-control-center gnome-tweak-tool gparted baobab gnome-system-log gnome-screenshot gnome-shell-extensions
+sudo apt install -y gdm3 gnome-panel gnome-control-center gnome-tweak-tool gparted baobab gnome-system-log gnome-screenshot gnome-shell-extensions gnome-icon-theme-symbolic
 sudo dpkg-reconfigure gdm3
 
