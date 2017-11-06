d491c62b1d6e4d7dd0b31c7149b167520ef9dec0
Pete Lewis <petele@unity3d.com>
2017-11-06 15:08:16 -0800

    Virtualbox

--- install-basics.sh
+++ install-basics.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-sudo apt-get install -y apt-file mercurial python-pip wget nano git ssh curl apcalc 
+sudo apt-get install -y apt-file mercurial python-pip wget nano git ssh curl apcalc virtualbox
