ba94baf4ed949c3dff8bc000f2f486aee7e3c53f
Pete Lewis <petele@unity3d.com>
2017-11-15 13:29:01 -0800

    Symbols

---
+++ init-symbols.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+echo "deb http://ddebs.ubuntu.com $(lsb_release -cs) main restricted universe multiverse
+deb http://ddebs.ubuntu.com $(lsb_release -cs)-updates main restricted universe multiverse
+deb http://ddebs.ubuntu.com $(lsb_release -cs)-security main restricted universe multiverse
+deb http://ddebs.ubuntu.com $(lsb_release -cs)-proposed main restricted universe multiverse" | \
+sudo tee -a /etc/apt/sources.list.d/ddebs.list
+sudo apt update
+
