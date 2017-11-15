ca20a172e381b22a37a88ea6f55da9af77aef777
Pete Lewis <petele@unity3d.com>
2017-11-15 14:40:20 -0800

    Install symbols for a given library:

--- install-symbols.sh
+++ install-symbols.sh
@@ -1,21 +1,29 @@
 #!/bin/bash
-PACKAGES=$(apt list --installed | grep -o -e "^[^/]*$1[^/]*")
-#PACKAGES=$(apt list --installed | grep -o -e "^.*$1.*/")
-if [[ "$PACKAGES" == "" ]]; then
-	echo "No packages found matching $1"
+SYMBOLPACKAGES=
+LIBSTOFIND=$1
+if [[ "$LIBSTOFIND" == "" ]]; then
+	echo "Usage: $0 <library or binary name>"
 	exit 1
 fi
-#IFS=$'\n'
-SYMBOLPACKAGES=
-for i in $PACKAGES; do
-	SYMBOLPKG=$i-dbgsym
-	SYMBOL=$(apt-cache search $SYMBOLPKG)
-	if [[ "$SYMBOL" != "" ]]; then
-		echo "Symbols for $i -> $SYMBOL"
-		SYMBOLPACKAGES=$(echo $SYMBOLPACKAGES $SYMBOLPKG)
-	else
-		echo "Symbols for $i not found."
+for pkg in "$LIBSTOFIND"; do
+	FOUNDPACKAGES=$(dpkg -S $pkg | grep -o -E "[a-zA-Z0-9_.-]+:[a-zA-Z0-9_.-]+")
+	#FOUNDPACKAGES=$(apt list --installed | grep -o -e "^[^/]*$pkg[^/]*")
+	if [[ "$FOUNDPACKAGES" == "" ]]; then
+		echo "No packages found matching file pattern $pkg"
+		exit 1
 	fi
+	for i in $FOUNDPACKAGES; do
+		PACKAGENAME=$(echo $i | grep -o -E "^[^:]+")
+		PACKAGEARCH=$(echo $i | grep -o -E ":.*$")
+		SYMBOLPKG=$PACKAGENAME-dbgsym
+		SYMBOL=$(apt-cache search $SYMBOLPKG)
+		if [[ "$SYMBOL" != "" ]]; then
+			echo "Symbols for $i -> $SYMBOL"
+			SYMBOLPACKAGES=$(echo $SYMBOLPACKAGES $SYMBOLPKG$PACKAGEARCH)
+		else
+			echo "Symbols for $PACKAGENAME$PACKAGEARCH not found."
+		fi
+	done
 done
 if [[ "$SYMBOLPACKAGES" != "" ]]; then
 	echo "Installing symbols for $SYMBOLPACKAGES"
