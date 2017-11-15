f8fec5f8d79466056cb79a7105cc38094860146e
Pete Lewis <petele@unity3d.com>
2017-11-15 13:40:39 -0800

    Symbol keys

--- init-symbols.sh
+++ init-symbols.sh
@@ -4,5 +4,6 @@ deb http://ddebs.ubuntu.com $(lsb_release -cs)-updates main restricted universe 
 deb http://ddebs.ubuntu.com $(lsb_release -cs)-security main restricted universe multiverse
 deb http://ddebs.ubuntu.com $(lsb_release -cs)-proposed main restricted universe multiverse" | \
 sudo tee -a /etc/apt/sources.list.d/ddebs.list
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 5FDFF622
 sudo apt update
 
