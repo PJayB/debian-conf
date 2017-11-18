32fe059382ba94af9d0667b1223d9cace238d2fe
Pete Lewis <mygit@pjblewis.com>
2017-11-17 16:00:12 -0800

    add sources repo entry

--- init-symbols.sh
+++ init-symbols.sh
@@ -3,7 +3,14 @@ echo "deb http://ddebs.ubuntu.com $(lsb_release -cs) main restricted universe mu
 deb http://ddebs.ubuntu.com $(lsb_release -cs)-updates main restricted universe multiverse
 deb http://ddebs.ubuntu.com $(lsb_release -cs)-security main restricted universe multiverse
 deb http://ddebs.ubuntu.com $(lsb_release -cs)-proposed main restricted universe multiverse" | \
-sudo tee -a /etc/apt/sources.list.d/ddebs.list
+sudo tee /etc/apt/sources.list.d/ddebs.list
+
+echo "deb-src http://us.archive.ubuntu.com/ubuntu $(lsb_release -cs) main restricted universe multiverse
+deb-src http://us.archive.ubuntu.com/ubuntu $(lsb_release -cs)-updates main restricted universe multiverse
+deb-src http://us.archive.ubuntu.com/ubuntu $(lsb_release -cs)-security main restricted universe multiverse
+deb-src http://us.archive.ubuntu.com/ubuntu $(lsb_release -cs)-proposed main restricted universe multiverse" | \
+sudo tee /etc/apt/sources.list.d/ubuntu-src.list
+
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 5FDFF622
 sudo apt-key update
 sudo apt update
