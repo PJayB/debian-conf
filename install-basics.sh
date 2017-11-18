8b5e7cb8bb6148a5728ac331c6a5fa7f0fc6aa63
Pete Lewis <mygit@pjblewis.com>
2017-11-18 21:13:17 +0000

    tmux

--- install-basics.sh
+++ install-basics.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-sudo apt-get install -y apt-file mercurial python-pip wget nano git ssh curl apcalc virtualbox vagrant gdb auditd ddd binutils tweak
+sudo apt-get install -y apt-file mercurial python-pip wget nano git ssh curl apcalc virtualbox vagrant gdb auditd ddd binutils tweak tmux
