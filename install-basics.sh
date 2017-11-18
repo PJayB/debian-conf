b5ae36a3d57d3ad4e37bb136038305f6199942dc
Pete Lewis <mygit@pjblewis.com>
2017-11-18 21:16:46 +0000

    build essential

--- install-basics.sh
+++ install-basics.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-sudo apt-get install -y apt-file mercurial python-pip wget nano git ssh curl apcalc virtualbox vagrant gdb auditd ddd binutils tweak tmux
+sudo apt-get install -y apt-file mercurial python-pip wget nano git ssh curl apcalc virtualbox vagrant gdb auditd ddd binutils tweak tmux gcc g++ make build-essential
+
