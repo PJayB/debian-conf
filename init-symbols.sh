d4b037f0c4fb92854a202fb0d378e47fcd2f723b
Pete Lewis <mygit@pjblewis.com>
2017-11-17 16:28:41 -0800

    Configure symbols for previous distros as well

--- init-symbols.sh
+++ init-symbols.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-echo "deb http://ddebs.ubuntu.com $(lsb_release -cs) main restricted universe multiverse
-deb http://ddebs.ubuntu.com $(lsb_release -cs)-updates main restricted universe multiverse
-#deb http://ddebs.ubuntu.com $(lsb_release -cs)-security main restricted universe multiverse
-deb http://ddebs.ubuntu.com $(lsb_release -cs)-proposed main restricted universe multiverse" | \
-sudo tee /etc/apt/sources.list.d/ddebs.list
-
 #DISTROS="precise quantal raring saucy trusty utopic vivid wily xenial yakkety zesty artful $(lsb_release -cs)"
 DISTROS="$(lsb_release -cs) trusty vivid xenial yakkety zesty artful"
 for distro in $DISTROS; do
-	echo "deb-src http://us.archive.ubuntu.com/ubuntu $distro main restricted universe multiverse
+	echo "# Symbols
+deb http://ddebs.ubuntu.com $distro main restricted universe multiverse
+deb http://ddebs.ubuntu.com $distro-updates main restricted universe multiverse
+#deb http://ddebs.ubuntu.com $distro-security main restricted universe multiverse
+deb http://ddebs.ubuntu.com $distro-proposed main restricted universe multiverse
+# Sources
+deb-src http://us.archive.ubuntu.com/ubuntu $distro main restricted universe multiverse
 deb-src http://us.archive.ubuntu.com/ubuntu $distro-updates main restricted universe multiverse
 #deb-src http://us.archive.ubuntu.com/ubuntu $distro-security main restricted universe multiverse
 deb-src http://us.archive.ubuntu.com/ubuntu $distro-proposed main restricted universe multiverse" | \
