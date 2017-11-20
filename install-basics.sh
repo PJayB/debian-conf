a66b155f51a227fa545fc9bb4a8c34326c3fb76e
Pete Lewis <mygit@pjblewis.com>
2017-11-20 10:02:23 -0800

    Rename basics

--- install-basics.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-sudo apt-get install -y apt-file mercurial python-pip wget nano git ssh curl apcalc virtualbox vagrant gdb auditd ddd binutils tweak tmux gcc g++ make build-essential cmake zip p7zip-full
-
