6c815ca7c7f45b41f752cce78d635bb694f53e4d
Pete Lewis <mygit@pjblewis.com>
2017-11-20 10:08:14 -0800

    Move VM-related installations to a new script

--- install-basics.sh
+++ install-basics.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-sudo apt-get install -y apt-file mercurial python-pip wget nano git ssh curl apcalc virtualbox vagrant gdb auditd ddd binutils tweak tmux gcc g++ make build-essential cmake zip p7zip-full screen
+sudo apt-get install -y apt-file mercurial python-pip wget nano git ssh curl apcalc gdb auditd ddd binutils tweak tmux gcc g++ make build-essential cmake zip p7zip-full screen
 
 
