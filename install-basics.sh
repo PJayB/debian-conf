bf5b783f186c4efe97372af74bc78b6964d8c485
Pete Lewis <petele@unity3d.com>
2018-01-04 14:11:33 -0800

    Configure tmux, lync, bashrc, ssh

--- install-basics.sh
+++ install-basics.sh
@@ -14,9 +14,26 @@ sudo apt-get install -y \
 	zip p7zip-full \
 	tmux screen
 
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+if [ ! -d ~/.ssh ]; then
+    mkdir ~/.ssh
+fi
+
 cp -nv config-templates/bash_aliases ~/.bash_aliases
 cp -nv config-templates/nanorc ~/.nanorc
 cp -nv config-templates/gitconfig ~/.gitconfig
 cp -nv config-templates/gdbinit ~/.gdbinit
+cp -nv config-templates/ssh-config ~/.ssh/config
+cp -nv config-templates/tmux.conf ~/.tmux.conf
 
+sudo cp -v config-templates/lynx.cfg /etc/lynx.cfg
 
+if ! grep -E 'basics-setup' ~/.bashrc; then
+    echo "Configuring bashrc"
+    cat config-templates/bashrc >> ~/.bashrc
+else
+    echo "bashrc already configured"
+fi
+
+. ~/.bashrc
