2d75bc86ab47af93283ab0083010143fc5cece6e
Pete Lewis <mygit@pjblewis.com>
2018-01-05 11:00:41 -0800

    Vagrant vbguest plugin

--- install-vagrant.sh
+++ install-vagrant.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 sudo apt install -y vagrant virtualbox
+vagrant plugin install vagrant-vbguest
 if [ ! -d ~/vagrants ]; then
     git clone git@work.bitbucket.org:petelewis-unity3d/vagrants.git ~/vagrants
 fi
