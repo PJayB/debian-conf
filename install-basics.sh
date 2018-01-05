47707e6e73d52820bf1f031888ffbb3de5198546
Pete Lewis <mygit@pjblewis.com>
2018-01-05 11:54:31 -0800

    Port install-basics to other package managers

--- install-basics.sh
+++ install-basics.sh
@@ -1,21 +1,50 @@
 #!/bin/sh
 set -e
 
-sudo apt-get install -y \
-    zsh \
-	apt-file \
-	mercurial python-pip \
-	git linux-tools-common \
-	wget curl ssh \
-	gdb binutils auditd \
-    valgrind kcachegrind \
-    linux-tools-$(uname -r) linux-cloud-tools-$(uname -r) \
-	gcc g++ make cmake build-essential \
-	nano tweak apcalc \
-	zip p7zip-full \
-	tmux screen \
-	ddate lolcat cmatrix cowsay toilet espeak \
-    htop
+PKGMAN=$(./package-manager.sh)
+if [ "$PKGMAN" = "" ]; then
+    echo "Unknown package manager."
+    exit 1
+fi
+
+SHARED_PACKAGES="zsh git wget curl tmux screen mercurial python-pip gdb binutils gcc g++ make cmake nano zip valgrind"
+#PERF_PACKAGES="auditd kcachegrind"
+DUMB_PACKAGES="ddate lolcat cmatrix cowsay toilet espeak"
+APT_PACKAGES="$SHARED_PACKAGES apt-file linux-tools-common linux-tools-$(uname -r) linux-cloud-tools-$(uname -r) build-essential tweak apcalc htop $DUMB_PACKAGES"
+RPM_PACKAGES="p7zip-plugins perf"
+YUM_PACKAGES="$SHARED_PACKAGES $RPM_PACKAGES p7zip-full epel-release"
+DNF_PACKAGES="$SHARED_PACKAGES $RPM_PACKAGES p7zip"
+
+sudo $PKGMAN update
+
+if [ "$PKGMAN" = "apt-get" ]; then
+    sudo $PKGMAN install -y $APT_PACKAGES
+elif [ "$PKGMAN" = "yum" ]; then
+    sudo $PKGMAN install -y $YUM_PACKAGES
+    sudo $PKGMAN groupinstall -y 'Development Tools'
+elif ["$PKGMAN" = "dnf" ]; then
+    sudo $PKGMAN install -y $DNF_PACKAGES
+    sudo $PKGMAN groupinstall -y 'Development Tools'
+else
+    echo "$PKGMAN-based distros aren't supported."
+    exit 1
+fi
+
+#sudo $PKGMAN install -y \
+#    zsh \
+#	apt-file \
+#	mercurial python-pip \
+#	git linux-tools-common \
+#	wget curl ssh \
+#	gdb binutils auditd \
+#    valgrind kcachegrind \
+#    linux-tools-$(uname -r) linux-cloud-tools-$(uname -r) \
+#	gcc g++ make cmake build-essential \
+#	nano tweak apcalc \
+#	zip p7zip-full \
+#	tmux screen \
+#	ddate lolcat cmatrix cowsay toilet espeak \
+#    htop
 
 if [ ! -d ~/.tmux/plugins/tpm ]; then
     echo "Getting tmux plugins..."
@@ -59,7 +88,7 @@ else
     echo "zshrc already configured"
 fi
 
-if [ ! -f ~/.ssh/id_rsa_* ]; then
+if [ ! -f ~/.ssh/config ]; then
     echo "Don't forget to set up your ssh keys!"
 fi
 
