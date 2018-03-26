265f3389bd1bb1b1ac9c96a47cbc238079f91b76
Pete Lewis <petele@unity3d.com>
2018-03-26 14:00:41 -0700

    Custom set mouse sensitivity using arg

--- deb/set-razer-mouse-sensitivity.sh
+++ deb/set-razer-mouse-sensitivity.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 DEVICES=$(xinput --list --short | grep -e "Razer.*pointer.*$")
+SENS=0.75
+if [ "$1" != "" ]; then
+    SENS=$1
+fi
 
 while read -r DEVICE; do
 	echo Found: $DEVICE
 	PTRID=$(echo $DEVICE | grep -o -e "id=[0-9]*" | sed 's/id=//g')
 #	echo $PTRID
 	xinput --set-prop $PTRID "libinput Accel Speed" -0.9
-	xinput --set-prop $PTRID "Coordinate Transformation Matrix" 0.6 0 0 0 0.6 0 0 0 2
+	xinput --set-prop $PTRID "Coordinate Transformation Matrix" $SENS 0 0 0 $SENS 0 0 0 2
 done <<< "$DEVICES"
 
 
