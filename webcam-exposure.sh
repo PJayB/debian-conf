f2cecd3582f79ed68353f05ebde202203911debc
Pete Lewis <petele@unity3d.com>
2018-03-28 10:09:45 -0700

    Better exposure control

--- webcam-exposure.sh
+++ webcam-exposure.sh
@@ -1,2 +1,5 @@
-v4l2-ctl --set-ctrl exposure_absolute=21
-v4l2-ctl --set-ctrl brightness=30
+#v4l2-ctl --set-ctrl exposure_absolute=21
+#v4l2-ctl --set-ctrl brightness=30
+v4l2-ctl --set-ctrl exposure_auto=1
+v4l2-ctl --set-ctrl exposure_absolute=20
+v4l2-ctl --set-ctrl brightness=200
