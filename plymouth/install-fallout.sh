673ccdad9b43aefc35c19b7e7f2468ba488f3035
Pete Lewis <mygit@pjblewis.com>
2019-03-25 10:56:45 -0700

    Update install-fallout.sh

--- plymouth/install-fallout.sh
+++ plymouth/install-fallout.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-# todo: unzip
-sudo update-alternatives --install 
-/usr/share/plymouth/themes/default.plymouth default.plymouth 
-/usr/share/plymouth/themes/fallout 100
+sudo update-alternatives --install /usr/share/plymouth/themes/default.plymouth default.plymouth /usr/share/plymouth/themes/fallout 100
+sudo update-initramfs -u
+
