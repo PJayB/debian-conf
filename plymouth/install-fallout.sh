9caf045a2cfe245671fcd1da402ed42385752527
Pete Lewis <mygit@pjblewis.com>
2019-03-26 08:30:26 -0700

    Ignore everything in this folder by default

--- plymouth/install-fallout.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-sudo update-alternatives --install /usr/share/plymouth/themes/default.plymouth default.plymouth /usr/share/plymouth/themes/fallout 100
-sudo update-alternatives --config default.plymouth
-sudo update-initramfs -u
-
