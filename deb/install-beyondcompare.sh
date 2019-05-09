f20470d1d65438a8cbe1882e14cae984ccca0d5d
Pete Lewis <pelewis@tesla.com>
2019-05-09 15:44:34 -0700

    Fix bcompare install script

--- deb/install-beyondcompare.sh
+++ deb/install-beyondcompare.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-wget https://www.scootersoftware.com/bcompare-4.2.9.23626_amd64.deb
+BCDEB=bcompare-4.2.9.23626_amd64.deb
+wget https://www.scootersoftware.com/$BCDEB
 sudo apt-get update
 sudo apt-get install gdebi-core
-sudo gdebi bcompare-4.2.3.22587_amd64.deb
+sudo gdebi $BCDEB
+
 
