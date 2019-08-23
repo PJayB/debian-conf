4262072885d91d7eac29a42045cecfef54dd9465
Pete Lewis <pelewis@tesla.com>
2019-08-22 19:39:04 -0700

    Updates to mouse sensitivity

--- set-razer-mouse-sensitivity.sh
+++ set-razer-mouse-sensitivity.sh
@@ -9,7 +9,8 @@ while read -r DEVICE; do
 	echo Found: $DEVICE
 	PTRID=$(echo $DEVICE | grep -o -e "id=[0-9]*" | sed 's/id=//g')
 #	echo $PTRID
-	xinput --set-prop $PTRID "libinput Accel Speed" -0.9
+#	xinput --set-prop $PTRID "libinput Accel Speed" -0.9
+    xinput --set-prop $PTRID "Device Accel Velocity Scaling" 3.0
 	xinput --set-prop $PTRID "Coordinate Transformation Matrix" $SENS 0 0 0 $SENS 0 0 0 2
 done <<< "$DEVICES"
 
