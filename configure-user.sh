c3cddec269d9ce9ca3117bc422169810c49a7bed
Pete Lewis <mygit@pjblewis.com>
2020-04-29 22:30:14 -0700

    Fix ctrl-arrow keys for Darwin terminal

--- configure-user.sh
+++ configure-user.sh
@@ -32,24 +32,22 @@ if [ ! -f ~/.ssh/config ]; then
     echo "Don't forget to set up your ssh keys!"
 fi
 
-#if [ "$TERM" != "cygwin" ]; then
-#    echo "If you want to change default shells, use \"chsh -s /path/to/shell\""
-#    cat /etc/shells
-#fi
+if [ "$(uname -s)" == "Darwin" ]; then
+    cp -v darwin/inputrc ~/.inputrc
+else
+    gsettings set org.gnome.desktop.interface gtk-enable-primary-paste false
+    gsettings set org.gnome.desktop.wm.preferences button-layout "':minimize,maximize,close'"
+    gsettings set org.gnome.settings-daemon.plugins.media-keys terminal "'<Super>t'"
+    gsettings set org.gnome.desktop.wm.keybindings switch-windows "['<Alt>Tab']"
+    gsettings set org.gnome.desktop.wm.keybindings switch-windows-backward "['<Shift><Alt>Tab']"
+    gsettings set org.gnome.desktop.wm.keybindings switch-applications-backward "['<Shift><Super>Tab']"
+    gsettings set org.gnome.desktop.wm.keybindings switch-applications "['<Super>Tab']"
+
+    dconf load /org/gnome/terminal/legacy/profiles:/ < config-templates/gnome-terminal-profiles.dconf
+fi
 
 if [ "$SHELL" = "/bin/bash" ]; then
     . ~/.bashrc
 elif [ "$SHELL" = "/bin/zsh" ]; then
     . ~/.zshrc
 fi
-
-gsettings set org.gnome.desktop.interface gtk-enable-primary-paste false
-gsettings set org.gnome.desktop.wm.preferences button-layout "':minimize,maximize,close'"
-gsettings set org.gnome.settings-daemon.plugins.media-keys terminal "'<Super>t'"
-gsettings set org.gnome.desktop.wm.keybindings switch-windows "['<Alt>Tab']"
-gsettings set org.gnome.desktop.wm.keybindings switch-windows-backward "['<Shift><Alt>Tab']"
-gsettings set org.gnome.desktop.wm.keybindings switch-applications-backward "['<Shift><Super>Tab']"
-gsettings set org.gnome.desktop.wm.keybindings switch-applications "['<Super>Tab']"
-
-dconf load /org/gnome/terminal/legacy/profiles:/ < config-templates/gnome-terminal-profiles.dconf
-
