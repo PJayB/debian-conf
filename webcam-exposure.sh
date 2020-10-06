6fa346f1f9e3a12eb4de89012578529660cbef59
Pete Lewis <mygit@pjblewis.com>
2020-10-06 08:49:51 -0700

    Webcam exposure takes argument

--- webcam-exposure.sh
+++ webcam-exposure.sh
@@ -1,5 +1,6 @@
 #v4l2-ctl --set-ctrl exposure_absolute=21
 #v4l2-ctl --set-ctrl brightness=30
+EXP=${1:-200}
 v4l2-ctl --set-ctrl exposure_auto=1
 v4l2-ctl --set-ctrl exposure_absolute=20
-v4l2-ctl --set-ctrl brightness=200
+v4l2-ctl --set-ctrl brightness=$EXP
