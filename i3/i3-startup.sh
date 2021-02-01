2148cf887372227fa3fd479d56040367619d39cb
Pete Lewis <mygit@pjblewis.com>
2021-02-01 10:01:46 -0800

    Don't start browser automatically on startup

--- i3/i3-startup.sh
+++ i3/i3-startup.sh
@@ -10,16 +10,17 @@ fi
 # Start pulseaudio
 start-pulseaudio-x11
 
+# Start a terminal
+i3-msg "workspace 1; layout stacking; exec i3-sensible-terminal;"
+#if xrandr --listactivemonitors | grep -qE '^ 1:' ; then
+#    i3-msg "workspace 2; layout stacking; exec google-chrome"
+#else
+#    i3-msg "exec google-chrome"
+#fi
+
 # Execute custom scripts
 CUSTOMSCRIPT=~/.config/i3/custom-startup.sh
 if [ -f $CUSTOMSCRIPT ]; then
     source "$CUSTOMSCRIPT"
 fi
 
-# Start a terminal
-i3-msg "workspace 1; layout stacking; exec i3-sensible-terminal;"
-if xrandr --listactivemonitors | grep -qE '^ 1:' ; then
-    i3-msg "workspace 2; layout stacking; exec google-chrome"
-else
-    i3-msg "exec google-chrome"
-fi
