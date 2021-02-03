9691d47ba8f1dce4d6628142d5142cbf97b440b9
Pete Lewis <mygit@pjblewis.com>
2021-02-03 12:13:25 -0800

    Local lock.sh

--- i3/lock.sh
+++ i3/lock.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-wpaper=$(gsettings get org.gnome.desktop.background picture-uri | sed -nr "s|'file://(.*)'|\1|p")
+wpaper="$HOME/.config/i3/lock-screen.png"
+#$(gsettings get org.gnome.desktop.background picture-uri | sed -nr "s|'file://(.*)'|\1|p")
 if [ -f "$wpaper" ]; then
     wpaper=("--image=$wpaper" "--tiling")
+else
+    wpaper=("--color=0C2D4A")
 fi
-i3lock --color=0C2D4A --show-failed-attempts --ignore-empty-password "${wpaper[@]}"
+i3lock --show-failed-attempts --ignore-empty-password "${wpaper[@]}"
